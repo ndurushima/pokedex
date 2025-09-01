a4d462eff2345f6e5782514241c3cd40eabcc620
ndurushima <ndu1994@gmail.com>
2025-09-01 14:42:19 -0500

    updated PokemonList.js to include the images and added some classnames to be able to style it

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 export default function PokemonList({ pokemon }) {
   return (
-    <div>
+    <div className="pokemon-grid">
         {pokemon.map(p => (
-            <div key={p}>{p}</div>
+            <div key={p.id} className="pokemon-card">
+              <img 
+                src={p.image}
+                alt={p.name}
+                loading="lazy"
+                onError={e => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${p.id}.svg`;
+                }}
+              />
+              <div className="pokemon-name">{p.name}</div>
+              <div className="pokemon-id">#{p.id}</div>
+            </div>
         ))}
     </div>
   )
