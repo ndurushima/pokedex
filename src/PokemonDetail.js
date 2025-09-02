b9f67b6525a6178d7dbedd33e13d5f5e34f88346
ndurushima <ndu1994@gmail.com>
2025-09-02 14:18:27 -0500

    realized height and weight for each pokemon was weird. Added functions to convert decimeters and hectagrams to feet/inches and lbs.

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { capitalize } from '@mui/material';
 
 export default function PokemonDetail() {
   const { id } = useParams();
@@ -14,16 +15,32 @@ export default function PokemonDetail() {
 
   if (!pokemon) return <p>Loading...</p>;
 
+  // ---- Conversions for Height and Weight ----
+  const heightMeters = pokemon.height /10; // decimeters to meters
+  const weightKg = pokemon.weight / 10; // hectograms to kilograms
+  const weightLbs = (weightKg * 2.20462).toFixed(1); // Kilograms to pounds
+
+  function toFeetInches(meters) {
+    const totalInches = meters * 39.3701;
+    const feet = Math.floor(totalInches / 12);
+    const inches = Math.round(totalInches % 12);
+    return `${feet}' ${inches}"`;
+  }
+
+  const heightFeetInches = toFeetInches(heightMeters);
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <h2>{pokemon.name} #{pokemon.id}</h2>
+      <h2>#{pokemon.id}</h2>
+      <h1>{capitalize(pokemon.name)}</h1>
       <img
         src={pokemon.sprites.other['official-artwork'].front_default}
         alt={pokemon.name}
         style={{ width: 200 }}
       />
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
+      <p><strong>Type:</strong> {pokemon.types.map(t => capitalize(t.type.name)).join(', ')}</p>
+      <p><strong>Height:</strong> {heightFeetInches}</p>
+      <p><strong>Weight:</strong> {weightLbs} lbs.</p>
 
       <button onClick={() => navigate('/')} className="back-button">Back to List</button>
     </div>
